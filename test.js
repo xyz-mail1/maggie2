3d261681bbc23fe9cde152f922b1d2b03f4f650b
xyz-mail1 <sunday.september@protonmail.com>
2024-06-21 17:22:23 +0530

    Revert "remove unnecesarry code"
    This reverts commit e1900f694257d42ceae2820807c4330824354f41.

--- test.js
+++ test.js
@@ -1,27 +1,10 @@
-const fs = require("fs");
-const path = require("path");
-
-function readdirSyncRecursive(directory) {
-  let results = [];
-  const list = fs.readdirSync(directory);
-
-  list.forEach((file) => {
-    const filePath = path.join(directory, file);
-    const stat = fs.statSync(filePath);
-
-    if (stat && stat.isDirectory()) {
-      results = results.concat(readdirSyncRecursive(filePath));
-    } else {
-      results.push(filePath);
-    }
+const justreddit = require("justreddit");
+(async () => {
+  const image = await justreddit.randomImageFromSub({
+    subReddit: "nsfwcosplay",
+    postGetLimit: 1,
+    sortType: "random",
   });
 
-  return results;
-}
-
-// Example usage:
-const directoryPath = `./src/`;
-const allFiles = readdirSyncRecursive(directoryPath);
-console.log(allFiles);
-
-/* import { NSFW } from "nsfwhub";  For EsModule */
+  console.log(image);
+})();
