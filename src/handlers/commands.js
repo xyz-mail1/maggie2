e1900f694257d42ceae2820807c4330824354f41
xyz-mail1 <sunday.september@protonmail.com>
2024-06-21 16:47:06 +0530

    remove unnecesarry code

--- src/handlers/commands.js
+++ src/handlers/commands.js
@@ -1,33 +1,32 @@
 const fs = require("fs");
 
 module.exports = (client) => {
-  let total = 0; // all commands loaded
+  let total = 0;
   const commandFolders = fs.readdirSync("./src/commands");
 
-  for (const folder of commandFolders) {
+  commandFolders.forEach((folder) => {
     const commandFiles = fs
       .readdirSync(`./src/commands/${folder}`)
       .filter((file) => file.endsWith(".js"));
-    let individual = 0; // commands loaded from foldeds
-    for (const file of commandFiles) {
+    let individual = 0;
+
+    commandFiles.forEach((file) => {
       try {
         const command = require(`../commands/${folder}/${file}`);
-
-        // Check if the command name already exists
         if (client.commands.has(command.name)) {
           client.logger.warn(`Duplicate command name: ${command.name}`);
-          continue; // Skip loading this command to avoid overwriting
+        } else {
+          client.commands.set(command.name, command);
+          total++;
+          individual++;
         }
-
-        client.commands.set(command.name, command);
-        total++;
-        individual++;
       } catch (err) {
         client.logger.error(`Failed to load ${file}. Reason: ${err.message}`);
       }
-    }
+    });
+
     client.logger.log(`Loaded ${individual} commands from ${folder}`);
-  }
+  });
 
   client.logger.log(`Loaded total ${total} commands`);
 };
