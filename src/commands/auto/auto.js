99952375922eb0d74d5396172489cfe19ab390c1
xyz-mail1 <sunday.september@protonmail.com>
2024-06-29 10:39:50 +0530

    add auto command + add cmd to autosend nekobot images

---
+++ src/commands/auto/auto.js
@@ -0,0 +1,24 @@
+const { EmbedBuilder } = require("discord.js");
+const nekobot = require("../../wrapper/neko");
+const api = new nekobot();
+module.exports = {
+  name: "auto",
+  SnM: true,
+  run: async (client, message, args) => {
+    const channel = message.channel.id;
+    async function autolewd() {
+      const image = await api.image(args);
+      const link = image.message;
+      const embed = new EmbedBuilder()
+        .setImage(link)
+        .setColor("LuminousVividPink");
+      client.channels.cache.get(channel).send({ embeds: [embed] });
+    }
+    try {
+      setInterval(autolewd, 60000);
+    } catch (err) {
+      message.channel.send({ content: `auto command stopped due to error` });
+      console.log(err);
+    }
+  },
+};
--- src/events/ready.js
+++ src/events/ready.js
@@ -4,10 +4,11 @@ module.exports = {
   name: Events.ClientReady,
   once: true,
   execute: async (client) => {
-    client.logger.log(`Ready! Logged in as ${client.user.tag}`);
-    client.logger.log("Guilds");
+    client.logger.log("Guilds: ");
     client.guilds.cache.forEach((guild) => {
       client.logger.log(`${guild.name} | ${guild.id}`);
     });
+
+    client.logger.log(`Ready! Logged in as ${client.user.tag}`);
   },
 };
